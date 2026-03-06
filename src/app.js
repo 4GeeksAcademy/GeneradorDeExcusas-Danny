62915f5e280b99091f596084bc20baefc0af3870
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-03-06 19:29:09 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let acction = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let rdmwho = Math.floor(Math.random() * who.length)
+let rdmacction = Math.floor(Math.random() * acction.length)
+let rdmwhat = Math.floor(Math.random() * what.length)
+let rdmexcusewhen = Math.floor(Math.random() * when.length)
+
+
+document.getElementById("excuse").innerHTML = (who[rdmwho] + " " + acction[rdmacction] + " " + what[rdmwhat] + " " + when[rdmexcusewhen]);
+
+
 };
